b92dc1ac3abd826a4ed90f47758718a8c383ddb0
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-01-07 18:48:15 -0500

    [add] projections!

--- src/main/java/org/spf4j/demo/ExampleResource.java
+++ src/main/java/org/spf4j/demo/ExampleResource.java
@@ -4,7 +4,11 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericRecord;
 import org.spf4j.demo.avro.DemoRecordInfo;
 
 /**
@@ -16,6 +20,11 @@ public interface ExampleResource {
   @GET
   @Produces(value = {"application/octet-stream", "application/json"})
   List<DemoRecordInfo> getRecords();
+
+  @GET
+  @Path("projection")
+  @Produces(value = {"application/octet-stream", "application/json"})
+  <T> List<GenericRecord> getRecordsProjection(@QueryParam("projection") Schema elementSchema);
 
   @POST
   @Consumes(value = {"application/octet-stream", "application/json"})
