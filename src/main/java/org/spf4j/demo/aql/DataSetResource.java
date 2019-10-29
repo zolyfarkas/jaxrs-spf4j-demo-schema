36cfad5349716f855d773a70461360dd80cb04b7
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-10-29 16:54:00 -0400

    [cleanup]

---
+++ src/main/java/org/spf4j/demo/aql/DataSetResource.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019 SPF4J.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spf4j.demo.aql;
+
+import com.google.common.reflect.TypeToken;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Type;
+import javax.ws.rs.GET;
+import javax.ws.rs.QueryParam;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.IndexedRecord;
+import org.apache.avro.specific.SpecificRecord;
+
+/**
+ * @author Zoltan Farkas
+ */
+public interface DataSetResource<T extends IndexedRecord> {
+
+  default Schema getSchema() {
+    Type[] intfs = this.getClass().getGenericInterfaces();
+    for (Type type : intfs) {
+      TypeToken<?> tt = TypeToken.of(type);
+      if (tt.getRawType() == DataSetResource.class) {
+        Class<?> srClasz = tt.getComponentType().getRawType();
+        if (SpecificRecord.class.isAssignableFrom(srClasz)) {
+          try {
+            return ((SpecificRecord) srClasz.getDeclaredConstructor().newInstance()).getSchema();
+          } catch (InstantiationException | IllegalAccessException | InvocationTargetException
+                  | NoSuchMethodException| SecurityException ex) {
+            throw new IllegalStateException("Invalid specific record " + srClasz);
+          }
+        }
+      }
+    }
+    throw new IllegalStateException("Resource must overwrite default implementation of getSchema()");
+  }
+
+  default String getName() {
+    return getSchema().getName();
+  }
+
+  @GET
+  Iterable<T> getData(@QueryParam("_where") String where,
+          @QueryParam("_select") String select);
+
+
+}
