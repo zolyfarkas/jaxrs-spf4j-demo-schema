2b397a4d8c2300de6e56c75efd1a23e462ff31d7
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-10-28 20:25:42 -0400

    [add] working on a avro sql example.

---
+++ src/main/java/org/spf4j/demo/aql/AvroQueryResource.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 SPF4J.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spf4j.demo.aql;
+
+import java.util.Map;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericRecord;
+import org.spf4j.jaxrs.StreamingArrayContent;
+
+/**
+ * REST avro sql endpoint.
+ * @author Zoltan Farkas
+ */
+public interface AvroQueryResource {
+
+  @GET
+  @Path("query")
+  StreamingArrayContent<GenericRecord> query(@QueryParam("query") String query);
+
+  @GET
+  @Path("schema")
+  Map<String, Schema> schema();
+
+  @GET
+  @Path("schema/{entityName}")
+  Schema entitySchema(@PathParam("entityName") String enttityName);
+
+}
