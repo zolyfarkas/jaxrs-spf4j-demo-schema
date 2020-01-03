66d272ae393238d96e004a51a4fd397fd4e0f6dd
Zoltan Farkas <zolyfarkas@yahoo.com>
2020-01-03 01:42:11 +0100

    [add] new field

--- src/main/java/org/spf4j/demo/ExampleResource.java
+++ src/main/java/org/spf4j/demo/ExampleResource.java
@@ -5,6 +5,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import org.apache.avro.Schema;
@@ -21,6 +22,12 @@ public interface ExampleResource {
   @Produces(value = {"application/avro", "application/avro-x+json", "application/octet-stream",
     "application/json", "text/csv"})
   List<DemoRecordInfo> getRecords();
+
+  @GET
+  @Produces(value = {"application/avro", "application/avro-x+json", "application/octet-stream",
+    "application/json"})
+  @Path("{id}")
+  DemoRecordInfo getRecord(@PathParam("id") String id);
 
   @GET
   @Path("projection")
